adedc79d6cb439774ec22e9602c52af06ce00350
susrith <susrith.maddi05@gmail.com>
2024-03-29 11:28:05 +0000

    2nd test case is not working

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const path = require('path')
+
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const app = express()
+const dbPath = path.join(__dirname, 'cricketTeam.db')
+let db = null
+const initialize = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server Running')
+    })
+  } catch (e) {
+    console.log(`DB Error:${e.message}`)
+    process.exit(1)
+  }
+}
+initialize()
+const convertDbObject = dbobject => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  }
+}
+app.get('/players/', async (req, res) => {
+  const books = `select * from cricket_team order by player_id;`
+  const allbooks = await db.all(books)
+  res.send(allbooks.map(eachPlayer => convertDbObject(eachPlayer)))
+})
+
+module.exports = app
